9a6311b8b49c0d529477c810a300a6856c823af1
Dave Ondona <ondonadave5@gmail.com>
2023-12-04 00:17:20 +0800

    Did some work on ContactPage.jsx, To Be Fixed.

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 import {Navbar, Nav } from "react-bootstrap";
 import {useNavigate ,Link } from "react-router-dom";
 import "./LandingPage.css"; 
+import "./C-Styling.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 export default function ContactPage() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
 
     let navigate = useNavigate();
 
@@ -15,6 +20,48 @@ export default function ContactPage() {
         navigate('/login'); 
     };
     
+    const sendMsg = async (e) => {
+      e.preventDefault();
+
+      if(!name.trim() || !email.trim() || !msg.trim()) {
+        alert("Please fill in all fields");
+        return;
+      }
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+          alert("Please enter a valid email address");
+          return;
+      }
+
+      const minWordCount = 5; 
+      if (msg.trim().split(/\s+/).length < minWordCount) {
+          alert(`Please enter a message with at least ${minWordCount} words`);
+          return;
+      }
+
+        try {
+          const response = await axios.post('http://localhost:8080/contact/insertMsg', {
+              contact_name: name, 
+              contact_email: email,   
+              contact_message: msg,
+          });
+
+          if (response.status === 200) { 
+              console.log(response.data);
+              alert('Message Sent!');
+              navigate('/'); 
+          } else {
+              console.log(response.data);
+              throw new Error('Unexpected Error');
+          }
+
+      } catch (err) {
+        console.error('Error:', err);
+        alert('Failed to send message. Please try again.');
+      }
+    }
+
     AOS.init(); 
     return (
       <div className="about-us">
@@ -39,7 +86,34 @@ export default function ContactPage() {
           
               <div data-aos="fade-up" className="container-1" style={{backgroundColor:"#ffffff"}}>Contact Us
               </div>
+              <div data-aos="fade-right" style={{display:"flex", flex:"3", paddingRight:"50px"}}>
+                <div style={{padding:"25px", backgroundColor:"#F5FCFF", display:"block"}}>
+                  <h5 className="heading" style={{marginRight:"120px"}}>Send us a Message!</h5>
+                  <input type="text" className="input-field" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}></input>
+                  <input type="text" className="input-field" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                  <textarea type="text" className="input-field" style={{paddingTop:"10px", paddingBottom:"50px"}} placeholder="Message" value={msg} onChange={(e) => setMsg(e.target.value)}></textarea>
+                  <button className="button" onClick={sendMsg}>SEND</button>
+                </div>
+                <div><hr></hr></div>
+                <div style={{marginRight:"50px", padding:"25px", backgroundColor:"#F5FCFF"}}>
+                  <h5 className="heading">Address</h5>
+                  <ul>
+                  <li style={{color:"#04364a", fontWeight:"0", fontFamily:"Robot-Regular, Helvetica"}}>N. Bacalso Avenue, Cebu City, Philippines</li>
+                  </ul>
+
+                  <div style={{paddingTop:"50px"}}>
+                    <h5 className="heading">Support</h5>
+                    <ul>
+                    <li style={{color:"#04364a", fontWeight:"0", fontFamily:"Robot-Regular, Helvetica"}}>energywise.support@energywise.com</li>
+                    <li style={{color:"#04364a", fontWeight:"0", fontFamily:"Robot-Regular, Helvetica"}}>+21 1744 868</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              
+             
         </div>
+
         </div>
 
 
