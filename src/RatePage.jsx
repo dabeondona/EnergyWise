ac22dbb7f67367530ec3a01d96e929800dc5be02
Dave Ondona <ondonadave5@gmail.com>
2023-12-05 09:40:16 +0800

    Modified LoginPage.jsx, staging initial code.

---
+++ src/RatePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function RatePage() {
+    return (
+        <h1>Hi</h1>
+    );
+}
--- src/Testpage.jsx
+++ src/Testpage.jsx
@@ -1,5 +1,10 @@
-export default function Testpage() {
+export default function TestPage() {
+    const userDetails = JSON.parse(localStorage.getItem('userDetails'));
+
     return (
-        <h1>hi</h1>
+        <div>
+            <h1>Welcome, {userDetails.firstName} {userDetails.lastName}</h1>
+            <p>Email: {userDetails.email}</p>
+        </div>
     );
-};
+}
