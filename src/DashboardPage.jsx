94e46c3e6ab1912c31f0c36c80093e94d4dd2e0c
Dave Ondona <ondonadave5@gmail.com>
2023-12-20 16:21:55 +0800

    second commit to the first commit

--- src/DashboardPage.jsx
+++ src/DashboardPage.jsx
@@ -160,6 +160,7 @@ export default function DashboardPage() {
                     <li><NavLink to="/login" onClick={handleLogout} activeClassName="active">Logout</NavLink></li>
                 </ul>
             </div>
+            
             <div style={{marginLeft:"300px"}}>
                 <div style={{display:"flex"}}>
                     <div style={{display:"block"}}>
